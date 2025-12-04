5ebf150ad3410d4ab927f77f583c5cfc1cf371f8
Ashutosh <ashutoshjosheee@gmail.com>
2025-12-04 11:08:05 +0530

    -Added method FillBoxInitialCordiantes() in Board class, Added player class properties

--- ApiChessWebApp/Player.cs
+++ ApiChessWebApp/Player.cs
@@ -2,5 +2,17 @@
 {
     public class Player
     {
+        //Player 1 can be black always
+        public Colors Color{ get; set; }
+        public List<List<Piece?>> Pieces { get; set; } = new List<List<Piece?>>(8);
+        public Player(){}
+        public Player(Colors color) 
+        {
+            this.Color = color;
+        }
+        public Player(Colors color, List<List<Piece?>> pieces)
+        {
+            this.Pieces = pieces;
+        }
     }
 }
